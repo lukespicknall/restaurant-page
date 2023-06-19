60eb9501e7c11776233f222cb10544593758d4a2
Luke Spicknall <lukejspicknall@gmail.com>
2023-06-19 18:18:20 -0400

    create and display connection between src/index.js and dist/index.html through webpack and its bundle.js

--- src/index.js
+++ src/index.js
@@ -1,1 +1,10 @@
-console.log('hi')
+console.log('hi');
+const page = document.getElementById('content')
+
+const test = () => {
+    const tester = document.createElement('div')
+    tester.innerText = 'test'
+    return tester;
+}
+
+page.appendChild(test());
