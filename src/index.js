dc34fb8a2309af07dc3bc3f79b43a3fa6eb61e60
Luke Spicknall <lukejspicknall@gmail.com>
2023-06-20 21:04:01 -0400

    added image and font rules to webpack.config.js. added font/ and images/ directories and added Roboto and placeholder icon. Imported icon to index.js and appended to DOM and imported Roboto font-face to style.css.

--- src/index.js
+++ src/index.js
@@ -1,12 +1,17 @@
-import './style.css'
+import './style.css';
+import Heart from './images/heart-outline.svg';
 
-const page = document.getElementById('content')
+const page = document.getElementById('content');
 
 const test = (a) =>{
     const tester = document.createElement('div')
     tester.classList.add('message')
     tester.innerText = a;
-    return tester
-}
 
-page.appendChild(test('Hey now!'))
+    const myHeart = new Image();
+    myHeart.src = Heart;
+    tester.appendChild(myHeart);
+    return tester;
+};
+
+page.appendChild(test('Hey now!'));
