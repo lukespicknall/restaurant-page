4a668129101d610e5101d5f8ee72ad303539fadd
Luke Spicknall <lukejspicknall@gmail.com>
2023-07-02 21:48:11 -0400

    create mobile responsive style for .menu module. major style.css update to this section.

--- src/menu.js
+++ src/menu.js
@@ -16,10 +16,10 @@ const createMenulist = () => {
     menuImage.classList.add('menu-image');
     const itemInfo = document.createElement('div');
     itemInfo.classList.add('item-info');
-    const itemTitle = document.createElement('h2');
+    const itemTitle = document.createElement('div');
     itemTitle.classList.add('item-title');
     itemTitle.textContent = b;
-    const menuDescription = document.createElement('p');
+    const menuDescription = document.createElement('div');
     menuDescription.classList.add('menu-description');
     menuDescription.textContent = c;
     itemInfo.appendChild(itemTitle);
@@ -37,10 +37,10 @@ const createMenulist = () => {
     menuImage.classList.add('menu-image');
     const itemInfo = document.createElement('div');
     itemInfo.classList.add('item-info');
-    const itemTitle = document.createElement('h2');
+    const itemTitle = document.createElement('div');
     itemTitle.classList.add('item-title');
     itemTitle.textContent = b;
-    const menuDescription = document.createElement('p');
+    const menuDescription = document.createElement('div');
     menuDescription.classList.add('menu-description');
     menuDescription.textContent = c;
     itemInfo.appendChild(itemTitle);
@@ -50,11 +50,11 @@ const createMenulist = () => {
     return item;
   };
 
-  const sandwich = menuItemEven(sandwichPack, 'Sandwich Pack Picnic', 'The go-to. Smoked beef and pork tenderloin sandwiches. Charcuterie board with unpasteurized goats milk cheese, apricot and gerkin. A carafe of our house lager');
+  const sandwich = menuItemEven(sandwichPack, 'Sandwich Picnic', 'The go-to. Smoked beef and pork tenderloin sandwiches. Charcuterie board with unpasteurized goats milk cheese, apricot and gerkin. A carafe of our house lager.');
   const pinot = menuItemOdd(pinotPack, 'The Pinot Pack', 'Our pinot pack is perfect for a more tradional spread. Luscious figs and grapes with creamy cows milk cheese. A bottle of Pinot Noir.');
-  const taco = menuItemEven(tacoPack, 'Taco Picnic', 'NEW! Our Taco picnic is the perpect pick for a splash of summer. Braised chiken tacos with lime and jalepeno. Tequila shots with fresh strawberry and lime');
-  const fruit = menuItemOdd(fruitPack, 'Fruit and Rose', 'Looking for something light and elegant? The fruit and rose picnic is where its at. Apple and rose.');
-  const snack = menuItemEven(snackPack, 'The Snack Pack', 'Just need something to nibble while you chew your grapes? Salty pretzels, fresh baked cookies and wine.');
+  const taco = menuItemEven(tacoPack, 'Taco Picnic', 'NEW! Our Taco picnic is the perpect pick for a splash of summer. Braised chiken tacos with lime and jalepeno. Tequila shots with fresh strawberry and lime.');
+  const fruit = menuItemOdd(fruitPack, 'Fruit and Rose', 'Looking for something light and elegant? The fruit and rose picnic is where its at. Apple, watermelon and rose.');
+  const snack = menuItemEven(snackPack, 'The Snack Pack', 'Just need something to nibble while you chew your grapes? Salty pretzels, fresh baked cookies, and strawberry.');
   menuList.appendChild(sandwich);
   menuList.appendChild(pinot);
   menuList.appendChild(taco);
