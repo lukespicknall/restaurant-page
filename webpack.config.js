5ba25d105f3d27fc6988201a2a30cabc72a27264
Luke Spicknall <lukejspicknall@gmail.com>
2023-07-04 19:18:13 -0400

    add favicon to the plugins[new HTMLWebpackPlugin] section of webpack.config.js. points to svg loaded in images directory.

--- webpack.config.js
+++ webpack.config.js
@@ -28,6 +28,7 @@ module.exports = {
     new HtmlWebpackPlugin({
       filename: "index.html",
       template: "./src/template.html",
+      favicon: "./src/images/utensils-solid.svg",
     }),
   ],
 
